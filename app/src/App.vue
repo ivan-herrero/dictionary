097846f6306da4ed267ba0895fff72771c6bd352
ivan-herrero <me@ivanherrero.dev>
2019-11-11 14:24:31 +0100

    Move app to app directory

---
+++ app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+}
+
+h1 {
+  margin: 0;
+}
+
+p {
+  text-align: justify;
+}
+
+#app {
+  height: 100vh;
+  padding: 30px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
